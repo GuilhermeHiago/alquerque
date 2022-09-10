38465d89d28f829ab9731eefe9068b30c3edecb4
Guilherme Santos <57719461+GuilhermeHiago@users.noreply.github.com>
2022-09-10 12:44:58 -0300

    Basic functions for alquerque game
    Add methods to create/clone board, get possible actions and apply actions to a board

--- alquerque.c
+++ alquerque.c
@@ -35,6 +35,13 @@ int min(int num1, int num2)
     return (num1 > num2 ) ? num2 : num1;
 }
 
+/**
+ * Receives:
+ *  int m (number of columms)
+ *  int n (number of rows)
+ * Returns:
+ *  int** (matrix of int filled with 0)
+*/
 int** createArray(int m, int n)
 {
     int* values = calloc(m*n, sizeof(int));
@@ -93,31 +100,44 @@ int show_board(int **board){
     printf("%c - %c - %c - %c - %c\n", board2[4][0], board2[4][1], board2[4][2], board2[4][3], board2[4][4]);
 }
 
-
+/**
+ * Receives:
+    int **state (board to verify)
+    int player (player value on board)
+    int *n_plays (addres of int var to save number of plays)
+ * Return:
+    Play * (Play[] with list of possible plays for a player)
+*/
 Play* get_possible_plays(int **state, int player, int *n_plays){
     // 5 pieces * max_of_8_plays = 40
-    Play *possibles[40];
+    Play *possibles = malloc(sizeof(Play) * 40);
     int count = 0;
+
     for(int i = 0; i < MAX_SIZE; i++){
         for(int j = 0; j < MAX_SIZE; j++){
-
+            
             if(state[i][j] != player) continue;
 
-            // in pair lines odd columms has diagonal moviment
+            // // in pair lines odd columms has diagonal moviment
             bool diagonal = i % 2 == j % 2;
-
-            for(int k = max(0, i-1); k < min(i+1, MAX_SIZE-1); k++){
-                for(int l = max(0, i-1); l < min(i+1, MAX_SIZE-1); l++){
-
+    
+            for(int k = max(0, i-1); k <= min(i+1, MAX_SIZE-1); k++){
+                for(int l = max(0, j-1); l <= min(j+1, MAX_SIZE-1); l++){
                     if(state[k][l] != 0){
                         continue;
                     }
 
-                    Play *move;
-                    move->x1 = i;
-                    move->y1 = j;
-                    move->x2 = k;
-                    move->y2 = l;
+                    if(!diagonal && (k == i-1 || k == i+1) && (l == j-1 || l == j+1)){
+                        continue;
+                    }
+
+            
+
+                    Play move;
+                    move.x1 = i;
+                    move.y1 = j;
+                    move.x2 = k;
+                    move.y2 = l;
                     possibles[count] = move;
                     count++;
                 }
@@ -125,10 +145,18 @@ Play* get_possible_plays(int **state, int player, int *n_plays){
         }
     }
 
-    return *possibles;
+    *n_plays += count + 1;
+
+    return possibles;
 }
 
-
+/*
+Receives:
+    int **board (the board to verify)
+    int player (player value)
+Return:
+    bool (if exist line of four pieces)
+*/
 bool is_final_state(int **board, int player){
 
     for(int i = 0; i < MAX_SIZE; i++){
@@ -139,25 +167,21 @@ bool is_final_state(int **board, int player){
 
             // down front diagonal
             if(i + 3 < MAX_SIZE && j + 3 < MAX_SIZE){
-                printf("aqui2\n");
                 if(board[i][j] && board[i+1][j+1] && board[i+2][j+2] && board[i+3][j+3])
                     return true;
             }
             // front row
             else if(i + 3 < MAX_SIZE){
-                printf("aqui\n");
                 if(board[i][j] && board[i+1][j] && board[i+2][j] && board[i+3][j])
                     return true;
             }
             // down back diagonal
             else if(i - 3 >= 0 && j + 3 < MAX_SIZE){
-                printf("aqui3\n");
                 if(board[i][j] && board[i-1][j+1] && board[i-2][j+2] && board[i-3][j+3])
                     return true;
             }
             // columm
             else if(j + 3 < MAX_SIZE){
-                printf("aqui4\n");
                 if(board[i][j] && board[i][j+1] && board[i][j+2] && board[i][j+3])
                     return true;
             }
@@ -168,15 +192,22 @@ bool is_final_state(int **board, int player){
     return false;
 }
 
+/*
+Receives: 
+    int matrix board 
+    Play play : play to apply
+Returns:
+    int **new_board (new pointer)
+*/
 int** apply_play(int **board, Play play){
 
     int **new_board = createArray(MAX_SIZE, MAX_SIZE);
     clone_matrix(board, new_board);
 
-    board[play.x2][play.y2] = board[play.x1][play.y1];
-    board[play.x1][play.y1] = 0;
+    new_board[play.x2][play.y2] = new_board[play.x1][play.y1];
+    new_board[play.x1][play.y1] = 0;
 
-    return board;
+    return new_board;
 }
 
 int main (int argc, const char * argv[]) 
@@ -188,6 +219,7 @@ int main (int argc, const char * argv[])
                           (int[]){0,0,0,0,0},
                           (int[]){-1,1,-1,1,-1}
     };
+
     Play p;
     p.x1 = 0;
     p.y1 = 0;
@@ -195,13 +227,16 @@ int main (int argc, const char * argv[])
     p.y2 = 1;
 
     int **board2 = apply_play(board, p);
-    show_board(board2);
-    printf("%d", is_final_state(board2, 1));
+    // show_board(board2);
+    show_board(board);
+    printf("%d\n", is_final_state(board2, 1));
 
-    int *n;
-    Play* poss = get_possible_plays(board, 1, n);
+    int n_plays = 0;
+    Play* poss = get_possible_plays(board, 1, &n_plays);
 
-    for(int i = 0; i < 40; i++) {
+    printf("number of plays: %d\n", n_plays);
+
+    for(int i = 0; i < n_plays-1; i++) {
         printf(" (%d,%d) -> (%d,%d), ", poss[i].x1, poss[i].y1, poss[i].x2, poss[i].y2);
     }
 
