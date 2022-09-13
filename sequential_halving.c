922b06e246747974927f6e9cfa7ff0bff7ec1a69
schererl <scherer.victor98@gmail.com>
2022-09-13 11:37:48 -0300

    seq_halving v2

--- sequential_halving.c
+++ sequential_halving.c
@@ -12,14 +12,13 @@ typedef struct{
 }node;
 
 int playout(int **state, int player){
-    srand(time(NULL));   // Initialization, should only be called once.
     
     int** playout_state = createArray(MAX_SIZE, MAX_SIZE);
     clone_matrix(state, playout_state);
     int curr_player = 1;
     
     while(is_terminal(playout_state)){
-        int n_plays;
+        int n_plays = 0;
         Play *possible_plays = get_possible_plays(playout_state, curr_player, &n_plays);
         curr_player *= -1;
 
@@ -36,5 +35,37 @@ void simulate(node n, int budget){
         n.reward+= playout(n.state, 1);
         n.n+= 1;
     }
+}
+
+Play sequential_halving(int **state, int budget){
+    srand(time(NULL));
+
+    int n_plays = 0;
+    Play *action_children = get_possible_plays(state, 1, &n_plays);
+    node *children = alloc(sizeof(node) * n_plays);
+
+    for(int i=0; i < n_plays;i++){
+        children[i].action = action_children[i];
+        children[i].state = apply_play(state, children[i].action);
+        children[i].n = 0;
+        children[i].reward = 0;
+    }
+
+    int layer_size = n_plays;
+    int layer_budget = budget;
+    while(layer_size > 1){
+        layer_budget = layer_budget/layer_size; //TODO
+        for(int i = 0; i < layer_size;i++){
+            child_budget = layer_budget/layer_size; //TODO
+            simulate(children[i], child_budget);
+        }
+        layer_size = (int)layer_budget/2;
+        sort(children, layer_budget);
+    }
+    Play action = children[0].action;
+    free(children);
+    free(action_children);
+    return children[0].action;
+
 }
 
