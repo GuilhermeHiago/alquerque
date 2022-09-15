eb5dfb44aab94ac328dc670d1fb96ef99e664a90
Guilherme Santos <57719461+GuilhermeHiago@users.noreply.github.com>
2022-09-15 10:45:32 -0300

    Add files via upload

--- sequential_halving.c
+++ sequential_halving.c
@@ -37,35 +37,93 @@ void simulate(node n, int budget){
     }
 }
 
-Play sequential_halving(int **state, int budget){
-    srand(time(NULL));
+// Play sequential_halving(int **state, int budget){
+//     srand(time(NULL));
 
-    int n_plays = 0;
-    Play *action_children = get_possible_plays(state, 1, &n_plays);
-    node *children = alloc(sizeof(node) * n_plays);
+//     int n_plays = 0;
+//     Play *action_children = get_possible_plays(state, 1, &n_plays);
+//     node *children = alloc(sizeof(node) * n_plays);
 
-    for(int i=0; i < n_plays;i++){
-        children[i].action = action_children[i];
-        children[i].state = apply_play(state, children[i].action);
-        children[i].n = 0;
-        children[i].reward = 0;
-    }
+//     for(int i=0; i < n_plays;i++){
+//         children[i].action = action_children[i];
+//         children[i].state = apply_play(state, children[i].action);
+//         children[i].n = 0;
+//         children[i].reward = 0;
+//     }
 
-    int layer_size = n_plays;
-    int layer_budget = budget;
-    while(layer_size > 1){
-        layer_budget = layer_budget/layer_size; //TODO
-        for(int i = 0; i < layer_size;i++){
-            child_budget = layer_budget/layer_size; //TODO
-            simulate(children[i], child_budget);
+//     int layer_size = n_plays;
+//     int layer_budget = budget;
+//     while(layer_size > 1){
+//         layer_budget = layer_budget/layer_size; //TODOlist
+//         sort(children, layer_budget);
+//     }
+//     Play action = children[0].action;
+//     free(children);
+//     free(action_children);
+//     return children[0].action;
+
+// }
+
+// Função de Ordenação por Seleção
+// Quick sort function
+void quick_sort(node *a, int left, int right) {
+    int i, j;
+    node x, y;
+     
+    i = left;
+    j = right;
+    x = a[(left + right) / 2];
+    float x_value = x.reward / x.n;
+     
+    while(i <= j) {
+        float node_i_value = a[i].reward / a[i].n;
+        float node_j_value = a[j].reward / a[j].n;
+
+        while(node_i_value > x_value && i < right) {
+            i++;
         }
-        layer_size = (int)layer_budget/2;
-        sort(children, layer_budget);
+        while(node_j_value < x_value && j > left) {
+            j--;
+        }
+        if(i <= j) {
+            y = a[i];
+            a[i] = a[j];
+            a[j] = y;
+            i++;
+            j--;
+        }
     }
-    Play action = children[0].action;
-    free(children);
-    free(action_children);
-    return children[0].action;
-
+     
+    if(j > left) {
+        quick_sort(a, left, j);
+    }
+    if(i < right) {
+        quick_sort(a, i, right);
+    }
 }
 
+int main(int argc, char const *argv[])
+{
+    node n1, n2, n3;
+
+    n1.n = 10;
+    n1.reward = 20;
+
+    n2.n = 20;
+    n2.reward = 20;
+
+    n3.n = 30;
+    n3.reward = 20;
+
+    node list[3] = {n1, n2, n3};
+
+    // list[0] = n1;
+
+    quick_sort(list, 0, 2);
+
+    for(int i = 0; i < 3; i++){
+        printf("pos %d n %d\n", i, list[i].n);
+    }
+    /* code */
+    return 0;
+}
