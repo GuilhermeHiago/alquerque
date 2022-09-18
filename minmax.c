b715925f0df17668d3249516eb275c3ef08f0657
RukNdf <48357253+RukNdf@users.noreply.github.com>
2022-09-18 20:48:58 -0300

    minmax

--- minmax.c
+++ minmax.c
@@ -5,6 +5,8 @@
 #endif
 
 #define NUMLINES 28
+#define MAXVALUE 100
+#define MAXWIDTH 1500
 
 
 /********************
@@ -71,10 +73,6 @@ Pos** generateLines(){
 	return lines;
 }
 
-void printPos(Pos p){
-	printf("x=%d y=%d | ", p.x, p.y);
-}
-
 int heuristic(int** board){
 	//total
 	int sum = 0;
@@ -103,7 +101,7 @@ int heuristic(int** board){
 		//at least two pieces in line
 		if(count > 1){
 			//won the game, return a value over maximum heuristic 
-			if(count == 4) return cur*(NUMLINES*4);
+			if(count == 4) return cur*MAXVALUE;
 			//otherwise add to sum 
 			sum += count*cur;
 		}
@@ -125,37 +123,155 @@ struct MMNode{
 	int player;
 	int value;	
 	int numChildren;
+	bool isFinal;
 	MMNode *children;
 };
 
+//expand current node
 void expandNode(MMNode *n){
+	if(n->isFinal) return;
 	int nextPlayer = -1*n->player;
 	
 	int nPlays = -1; ////////////%FIXME%
-	Play* nextPlays = get_possible_plays(n->board, nextPlayer, &nPlays);
+	Play* nextPlays = get_possible_plays(n->board, n->player, &nPlays);
 	n->children = malloc(nPlays*sizeof(MMNode));
 	n->numChildren = nPlays;
-	printf("%d\n", n->numChildren);
 	for(int i = 0; i < nPlays; i++){
 		Play p = nextPlays[i];
 		int **b = apply_play(n->board, p);
 		n->children[i].lastPlay = p;
 		n->children[i].board = b;
 		n->children[i].player = nextPlayer;
-		n->children[i].value = heuristic(b);
+		int value = heuristic(b);
+		n->children[i].value = value;
+		//final state
+		if(value == MAXVALUE || value == MAXVALUE*-1){
+			n->children[i].isFinal = true;
+		}else{
+			n->children[i].isFinal = false;
+		}
 		n->children[i].numChildren = 0;
 		n->children[i].children = NULL;		
 	}
 }
 
+//expand until depth
+void expandTo(int depth, MMNode *root){
+	//nodes to expand on cur depth
+	MMNode **curD = calloc(MAXWIDTH, sizeof(MMNode*));
+	//nodes on next depth 
+	MMNode **nextD = calloc(MAXWIDTH, sizeof(MMNode*));
+	//number of nodes on cur depth
+	int curN = 1;
+	
+	curD[0] = root;	
+	for(int d = 0; d < depth; d++){
+		//expand nodes on current depth
+		for(int i = 0; i < curN; i++){
+			if(curD[i]->isFinal) continue;
+			expandNode(curD[i]);
+		}
+		//add children to nextD
+		int nextN = 0;
+		for(int i = 0; i < curN; i++){
+			for(int j = 0; j < curD[i]->numChildren; j++){
+				nextD[nextN] = &(curD[i]->children[j]);
+				nextN++;
+			}	
+		}
+		//next to cur
+		MMNode **aux = curD;
+		curD = nextD;
+		nextD = aux;
+		//nextN is now curN
+		curN = nextN;
+	}
+	free(curD);
+	free(nextD);
+	printf("[NEXT DEPTH REQUIRES MAXWIDTH>=%d]\n", curN);
+}
+
+//send values up the tree
+int recalculate(MMNode *root){
+	//final node
+	if(root->numChildren == 0) return root->value;
+	int values[root->numChildren];
+	//recalculate children
+	for(int i = 0; i < root->numChildren; i++){
+		values[i] = recalculate(&(root->children[i]));
+	}
+	int value;
+	value = values[0];
+	//get max 
+	if(root->player == 1){
+		for(int i = 1; i < root->numChildren; i++){
+			if(values[i] > value) value = values[i];
+		}
+	}
+	//get min
+	else{
+		for(int i = 1; i < root->numChildren; i++){
+			if(values[i] < value) value = values[i];
+		}
+	}
+	root->value = value;
+	return value;
+}
+
+//cleanup
+void freeTree(MMNode *root){
+	if(root->numChildren == 0) return;
+	//free children of children
+	for(int i = 0; i < root->numChildren; i++){
+		freeTree(&(root->children[i]));
+	}
+	//free children
+	free(root->children);
+}
+
+//get next play
+Play minmax(int **board, int player, int depth){
+	//root node
+	MMNode root; 
+	root.board = board; root.value = 0; root.player=1; root.numChildren=0; root.isFinal = false;
+	
+	//expand tree
+	expandTo(depth, &root);
+	//recalculate values and get the max/min
+	int value = recalculate(&root);
+	
+	//find first max/min and get next play
+	Play play; 
+	for(int i = 0; i < root.numChildren; i++){
+		if(root.children[i].value == value){
+			play = root.children[i].lastPlay;
+			break;
+		}
+	}
+	
+	//free tree
+	freeTree(&root);
+	
+	return play;
+}
+
+
+/*****************************
+*	TEST
+*****************************/
 void printChildren(MMNode n){
-	printf("_|F (Player:%d Children:%d)\n", n.player, n.numChildren);
+	printf("_|F (Player:%d Children:%d Value:%d)\n", n.player, n.numChildren, n.value);
 	for(int i = 0; i < n.numChildren; i++){
 		MMNode node = n.children[i];
 		Play p = node.lastPlay;
 		printf("| [(%d, %d) -> (%d, %d)] (Player:%d Value:%d Children:%d)\n", p.x1, p.y1, p.x2, p.y2, node.player, node.value, node.numChildren);
 	}
 }
+
+void printPos(Pos p){
+	printf("x=%d y=%d | ", p.x, p.y);
+}
+
 
 int main(){
 	minMaxInit();
@@ -168,9 +284,39 @@ int main(){
 	};
 	show_board(board);
 	
-	MMNode testNode; testNode.board = board; testNode.player = 1;
-	expandNode(&testNode);
-	printf("\n");
+	/*MMNode testNode; testNode.board = board; testNode.value = 0; testNode.player = 1; testNode.numChildren = 0; testNode.isFinal = false;
+	
+	
+	printf("\nDEPTH=0\n");
 	printChildren(testNode);
+	
+	printf("\nDEPTH=1\n");
+	expandTo(1, &testNode);	
+	printChildren(testNode);
+	printChildren(testNode.children[0]);
+	
+	printf("\nDEPTH=2\n");
+	expandTo(2, &testNode);
+	printChildren(testNode);
+	printChildren(testNode.children[0]);
+	printChildren(testNode.children[0].children[0]);
+	
+	printf("\nDEPTH=3\n");
+	expandTo(3, &testNode);
+	printChildren(testNode);
+	printChildren(testNode.children[0]);
+	printChildren(testNode.children[0].children[0]);
+	
+	printf("\nRECALCULATE\n");
+	recalculate(&testNode);
+	printChildren(testNode);
+	freeTree(&testNode);
+	printf("\n");*/
+	
+	int player = 1;
+	int depth = 4;
+	Play next = minmax(board, player, depth);
+	printf("Next play: (%d,%d)->(%d,%d)", next.x1, next.y1, next.x2, next.y2);
+	
 	return 0;
 }
